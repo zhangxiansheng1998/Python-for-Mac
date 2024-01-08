54b1df4355544d24fb8d96196addc784a51dc79f
zhangxiansheng <1120620374@qq.con>
2024-01-08 08:44:38 +0800

    Initial commit

--- WEditor+WDA/app_case/open_fuda.py
+++ WEditor+WDA/app_case/open_fuda.py
@@ -2,6 +2,7 @@ import wda
 
 # 终端需要运行命令 tidevice -u [设备的udid] wdaproxy -B [手机上webdriveragent的bundleId] --port 8100
 # tidevice -u 9dde9a1c9c3c01a1cbac0dd36339278a527f7235 wdaproxy -B com.facebook.WebDriverAgentRunnerForZhangXianSheng.xctrunner --port 8100
+# tidevice -u 00008110-000155691AF2801E wdaproxy -B com.facebook.WebDriverAgentRunnerForZhangXianSheng.xctrunner --port 8100
 
 c = wda.Client('http://localhost:8100') # 8100为启动WDA设置的端口号
 
--- WEditor+WDA/app_case/test_app.py
+++ WEditor+WDA/app_case/test_app.py
@@ -2,6 +2,7 @@ import wda
 import unittest
 from time import sleep
 
+
 class TestCase(unittest.TestCase):
 
     @classmethod
@@ -10,7 +11,6 @@ class TestCase(unittest.TestCase):
         # tidevice -u 9dde9a1c9c3c01a1cbac0dd36339278a527f7235 wdaproxy -B com.facebook.WebDriverAgentRunnerForZhangXianSheng.xctrunner --port 8100
         # python3 -m weditor
         cls.c = wda.Client('http://localhost:8100')  # 8100为启动WDA设置的端口号
-
 
     @classmethod
     def tearDownClass(cls):
@@ -27,10 +27,10 @@ class TestCase(unittest.TestCase):
         self.c.xpath('//*[@label="登录/注册"]').click()
         sleep(3)
         self.c.xpath('//*[@label="pages/login/code/code[3]"]/StaticText[3]').set_text('1\n')
-        self.c.tap(100,200).set_text('1\n') # 通过X、Y轴坐标定位元素
+        self.c.tap(100, 200).set_text('1\n')  # 通过X、Y轴坐标定位元素
         self.c.xpath('//*[@label=""]').click()
-        #self.c.xpath('//*[@label="pages/login/selectAccount/selectAccount[4]"]/Other[1]').click()
+        # self.c.xpath('//*[@label="pages/login/selectAccount/selectAccount[4]"]/Other[1]').click()
+
 
 if __name__ == '__main__':
-
     unittest.main()
