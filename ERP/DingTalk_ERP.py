01c821800c49d105d8c52ea890a2c84a25770980
zhangxiansheng <1120620374@qq.con>
2024-03-06 15:14:18 +0800

    Changes

---
+++ ERP/DingTalk_ERP.py
@@ -0,0 +1,82 @@
+import os
+import re
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+import jenkins
+import json
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+
+timestamp = str(round(time.time() * 1000))
+secret = 'SEC26269f4538ec31adf200209b4222406ffe11bf212a55834361df95794ebf4716' #替换成自己的值
+secret_enc = secret.encode('utf-8')
+string_to_sign = '{}\n{}'.format(timestamp, secret)
+string_to_sign_enc = string_to_sign.encode('utf-8')
+hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+# print(timestamp)
+# print(sign)
+#钉钉通知的接口地址
+url = f'https://oapi.dingtalk.com/robot/send?access_token=4577151a48816d922f0b7973caaeef7828fd456c2a1e78cce1a85af1122aeb99&timestamp={timestamp}&sign={sign}' #钉钉群的token
+
+
+#利用python发送钉钉  在钉钉需要点击 查看测试报告的话 就需要把报告发送到服务器 再把地址参数传下面  就去掉jenkins所有操作
+# jenkins登录地址
+jenkins_url = "http://203.189.205.234:8888/"#Jenkins地址
+# 获取jenkins对象
+server = jenkins.Jenkins(jenkins_url, username='admin', password='3c778ef3c32842d9b386502915031ab5') #Jenkins登录名 ，密码
+# job名称
+job_name = "job/ERP/" #Jenkins运行任务名称
+# job的url地址
+job_url = jenkins_url + job_name
+# 获取最后一次构建
+job_last_build_url = server.get_info(job_name)['lastBuild']['url']
+# 报告地址
+report_url = job_last_build_url
+
+'''
+钉钉推送方法：
+读取result.txt文件内的数据，循环遍历获取需要的值
+使用钉钉机器人的接口，拼接后推送消息
+'''
+
+def DingTalkSend():
+    #服务器地址路径/var/lib/jenkins/workspace/ERP/order.txt
+    #本地地址路径order.txt
+    with open('order.txt', 'r', encoding='utf-8') as file:
+        # 读取文件内容
+        content = file.read()
+
+    # 钉钉推送
+    con = {"msgtype": "text",
+           "text": {
+               "content": "ERP-成功订单"
+                          "\n测试概述:" + "\n"
+                          "\n" + content +
+                          "\n构建地址：\n" + job_url +
+                          "\n订单地址：\n" + "http://h.thinkermen.com/wincc_xingeercc/mini2021_1.7.4/index.php?r=order/factory\n"
+           },
+           "at": {
+               "atMobiles": [
+                   "15180381485"  # 如果需要@某人，这里写他的手机号
+               ],
+               "isAtAll": 1  # 如果需要@所有人，这些写1
+           }
+
+           }
+    try:
+        urllib3.disable_warnings()
+        http = urllib3.PoolManager()
+        jd = json.dumps(con)
+        jd = bytes(jd, 'utf-8')
+        http.request('POST', url, body=jd, headers={'Content-Type': 'application/json'})
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    DingTalkSend()
--- ERP/generate_order_main.py
+++ ERP/generate_order_main.py
@@ -16,15 +16,14 @@ while True:
 
 
 class TestCase(unittest.TestCase):
-
     obj = None
     driver = None
 
     @classmethod
     def setUpClass(cls):
         print('\n程序开始')
-        cls.driver = webdriver.Chrome(options=Browser().browser_ui()) # 带UI界面启动
-        #cls.driver = webdriver.Chrome(options=Browser().browser_headless())  # 无头模式启动
+        #cls.driver = webdriver.Chrome(options=Browser().browser_ui())  # 带UI界面启动
+        cls.driver = webdriver.Chrome(options=Browser().browser_headless())  # 无头模式启动
         cls.obj = BasePage(cls.driver)
         cls.obj.implicitly_wait(15)
 
@@ -48,65 +47,68 @@ class TestCase(unittest.TestCase):
     def test_2_generate_order(self):
         """生成订单"""
         self.obj.wait(5)
-        for i in range(total_orders):
-            myTime = time.strftime("%Y-%m-%d~%H-%M-%S")
-            self.obj.click((By.ID, 'add_order'))
-            self.obj.switch_to_newest_window()
-            self.obj.explicitly_wait((By.XPATH, '/html/body/div[16]/div[2]/div[3]/ul/li[2]/div'), 10)
-            self.obj.click((By.XPATH, '/html/body/div[16]/div[2]/div[3]/ul/li[2]/div'))
-            self.obj.switch_to_newest_window()
-            self.obj.explicitly_wait((By.ID, 'customer_name'), 10)
-            self.obj.input((By.ID, 'customer_name'), "倪浩平")
-            self.obj.wait(2)
-            self.obj.click((By.XPATH, '/html/body/form/div[1]/div/div[1]/div[1]/div[2]/div/dl[2]/dd/div/ul/li'))
-            self.obj.explicitly_wait((By.NAME, 'zonggao'), 10)
-            self.obj.input((By.NAME, 'zonggao'), "1000")
-            self.obj.input((By.NAME, 'zongkuan'), "200")
-            self.obj.input((By.NAME, 'shuliang'), "1")
-            self.obj.input((By.NAME, 'yanse'), "黑色")
-            # 生成1到3元之间的随机浮点数,保留小数点后2位
-            random_number = round(random.uniform(1, 3), 2)
-            self.obj.input((By.NAME, 'danjia'), "{}".format(random_number))
-            self.obj.click((By.ID, 'start_count'))
-            self.obj.explicitly_wait((By.ID, 'save_order'), 10)
-            self.obj.click((By.ID, 'save_order'))
-            self.obj.explicitly_wait((By.CSS_SELECTOR,
-                                      '#order_factory > div.aui_state_focus.aui_state_lock > div > table > tbody > '
-                                      'tr:nth-child(2) > td.aui_c > div > table > tbody > tr:nth-child(2) > td.aui_main > '
-                                      'div > div > div.system_overlay_btn > button.sbt_white.fr.mr10.group_orders'), 10)
-            self.obj.click((By.CSS_SELECTOR,
-                            '#order_factory > div.aui_state_focus.aui_state_lock > div > table > tbody > '
-                            'tr:nth-child(2) > td.aui_c > div > table > tbody > tr:nth-child(2) > td.aui_main > '
-                            'div > div > div.system_overlay_btn > button.sbt_white.fr.mr10.group_orders'))
-            self.obj.switch_to_newest_window()
-            self.obj.explicitly_wait((By.XPATH, '//*[@id="rows"]/div[2]'), 10)
-            ul_num = self.obj.get_ul_number((By.XPATH, '//*[@id="rows"]/div[2]'))
-            """翻页处理"""
-            if 0 < ul_num < 30:
-                self.obj.click((By.XPATH, f'//*[@id="rows"]/div[2]/ul[{ul_num}]/li[4]/a'))
+        with open('order.txt', 'w') as file:
+            for i in range(total_orders):
+                myTime = time.strftime("%Y-%m-%d~%H-%M-%S")
+                self.obj.click((By.ID, 'add_order'))
                 self.obj.switch_to_newest_window()
-                self.obj.wait(5)
-                self.obj.erp_order_screenshot(myTime)
+                self.obj.explicitly_wait((By.XPATH, '/html/body/div[16]/div[2]/div[3]/ul/li[2]/div'), 10)
+                self.obj.click((By.XPATH, '/html/body/div[16]/div[2]/div[3]/ul/li[2]/div'))
                 self.obj.switch_to_newest_window()
-                self.obj.explicitly_wait((By.XPATH, '/html/body/div[81]/div/li'), 10)
-                self.obj.click((By.XPATH, '/html/body/div[81]/div/li'))
-
-            if ul_num >= 30:
-                self.obj.click((By.CSS_SELECTOR, '#pager > div.jPag-control-front > a'))
-                print('\n当前页面订单大于30个，正在跳转最新页面')
+                self.obj.explicitly_wait((By.ID, 'customer_name'), 10)
+                self.obj.input((By.ID, 'customer_name'), "倪浩平")
+                self.obj.wait(2)
+                self.obj.click((By.XPATH, '/html/body/form/div[1]/div/div[1]/div[1]/div[2]/div/dl[2]/dd/div/ul/li'))
+                self.obj.explicitly_wait((By.NAME, 'zonggao'), 10)
+                self.obj.input((By.NAME, 'zonggao'), "1000")
+                self.obj.input((By.NAME, 'zongkuan'), "200")
+                self.obj.input((By.NAME, 'shuliang'), "1")
+                self.obj.input((By.NAME, 'yanse'), "黑色")
+                # 生成1到3元之间的随机浮点数,保留小数点后2位
+                random_number = round(random.uniform(1, 3), 2)
+                self.obj.input((By.NAME, 'danjia'), "{}".format(random_number))
+                self.obj.click((By.ID, 'start_count'))
+                self.obj.explicitly_wait((By.ID, 'save_order'), 10)
+                self.obj.click((By.ID, 'save_order'))
+                self.obj.explicitly_wait((By.CSS_SELECTOR,
+                                          '#order_factory > div.aui_state_focus.aui_state_lock > div > table > tbody > '
+                                          'tr:nth-child(2) > td.aui_c > div > table > tbody > tr:nth-child(2) > td.aui_main > '
+                                          'div > div > div.system_overlay_btn > button.sbt_white.fr.mr10.group_orders'), 10)
+                self.obj.click((By.CSS_SELECTOR,
+                                '#order_factory > div.aui_state_focus.aui_state_lock > div > table > tbody > '
+                                'tr:nth-child(2) > td.aui_c > div > table > tbody > tr:nth-child(2) > td.aui_main > '
+                                'div > div > div.system_overlay_btn > button.sbt_white.fr.mr10.group_orders'))
+                self.obj.switch_to_newest_window()
                 self.obj.explicitly_wait((By.XPATH, '//*[@id="rows"]/div[2]'), 10)
                 ul_num = self.obj.get_ul_number((By.XPATH, '//*[@id="rows"]/div[2]'))
-                self.obj.click((By.XPATH, f'//*[@id="rows"]/div[2]/ul[{ul_num}]/li[4]/a'))
-                self.obj.switch_to_newest_window()
-                self.obj.wait(5)
-                self.obj.erp_order_screenshot(myTime)
-                self.obj.switch_to_newest_window()
-                self.obj.explicitly_wait((By.XPATH, '/html/body/div[81]/div/li'), 10)
-                self.obj.click((By.XPATH, '/html/body/div[81]/div/li'))
+                """翻页处理"""
+                if 0 < ul_num < 30:
+                    self.obj.click((By.XPATH, f'//*[@id="rows"]/div[2]/ul[{ul_num}]/li[4]/a'))
+                    self.obj.switch_to_newest_window()
+                    self.obj.wait(5)
+                    self.obj.erp_order_screenshot(myTime)
+                    self.obj.switch_to_newest_window()
+                    self.obj.explicitly_wait((By.XPATH, '/html/body/div[81]/div/li'), 10)
+                    self.obj.click((By.XPATH, '/html/body/div[81]/div/li'))
 
-            print("\n第", i + 1, "笔订单")
-            print("\n订单金额:", random_number, "元")
-            print("\n截图时间:", myTime)
+                if ul_num >= 30:
+                    self.obj.click((By.CSS_SELECTOR, '#pager > div.jPag-control-front > a'))
+                    print('\n当前页面订单大于30个，正在跳转最新页面')
+                    self.obj.explicitly_wait((By.XPATH, '//*[@id="rows"]/div[2]'), 10)
+                    ul_num = self.obj.get_ul_number((By.XPATH, '//*[@id="rows"]/div[2]'))
+                    self.obj.click((By.XPATH, f'//*[@id="rows"]/div[2]/ul[{ul_num}]/li[4]/a'))
+                    self.obj.switch_to_newest_window()
+                    self.obj.wait(5)
+                    self.obj.erp_order_screenshot(myTime)
+                    self.obj.switch_to_newest_window()
+                    self.obj.explicitly_wait((By.XPATH, '/html/body/div[81]/div/li'), 10)
+                    self.obj.click((By.XPATH, '/html/body/div[81]/div/li'))
+
+                print("\n第", i + 1, "笔订单")
+                print("\n订单金额:", random_number, "元")
+                print("\n截图时间:", myTime)
+
+                file.write(f"\n第{i + 1}笔订单\n订单金额: {random_number}元\n截图时间: {myTime}\n")
 
         print("\n总共生成", total_orders, "个订单")
         print("\n订单地址", 'http://h.thinkermen.com/wincc_xingeercc/mini2021_1.7.4/index.php?r=order/factory')
