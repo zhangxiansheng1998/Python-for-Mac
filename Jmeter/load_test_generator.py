32bfe6d0e130db3e8476cbea22f9025cbdc80b5d
zhangxiansheng <1120620374@qq.con>
2024-03-11 13:54:26 +0800

    Changes

--- Jmeter/load_test_generator.py
+++ Jmeter/load_test_generator.py
@@ -1,18 +1,46 @@
 import os
+import re
 import time
 from pathlib import Path
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 
-def run():
-    Mytime = time.strftime("%Y-%m-%d~%H-%M-%S")
+
+def modify_jmx_file(file_path, new_num_threads):
+    # 读取JMX文件内容
+    with open(file_path, 'r') as file:
+        jmx_content = file.read()
+
+    # 使用正则表达式匹配并替换数字
+    pattern = r'<stringProp name="ThreadGroup.num_threads">(\d+)</stringProp>'
+    replacement = f'<stringProp name="ThreadGroup.num_threads">{new_num_threads}</stringProp>'
+    modified_content = re.sub(pattern, replacement, jmx_content, count=1)
+
+    # 将修改后的内容写回文件
+    with open(file_path, 'w') as file:
+        file.write(modified_content)
+
+    print(f"已成功将线程数量修改为：{new_num_threads}")
+
+
+def run(filename):
     print("程序开始运行")
-    print("请选择您要运行的jmx脚本：")
-    root = Tk()
-    root.withdraw()
-    filename = askopenfilename()
-    print("当前文件：", filename)
-    jmx_path = "/Volumes/Disk/apache-jmeter-5.1.1/jmeter/jmx"
+    answer = input("是否需要修改线程数【是/否】")
+    if answer == "是":
+        try:
+            # 尝试获取用户输入的数字
+            num_threads = int(input("请输入新的线程数："))
+            # 如果输入成功，则更新线程数
+            modify_jmx_file(filename, num_threads)
+        except ValueError:
+            # 如果输入的不是数字，则捕获异常并打印错误信息
+            print("输入的不是一个有效的数字，线程数保持不变。")
+    elif answer == "否":
+        # 如果用户输入否，则不修改线程数，并打印信息
+        print("不修改线程数")
+    else:
+        # 如果用户输入既不是是也不是否，则打印提示信息
+        print("输入有误，请输入是或否")
+
+    Mytime = time.strftime("%Y-%m-%d~%H-%M-%S")
     path = "{}".format(filename)
 
     # 判断文件是否存在
@@ -20,46 +48,60 @@ def run():
         if os.path.exists(path):
             os.system(
                 "jmeter -n -t {} -l /Volumes/Disk/apache-jmeter-5.1.1/jmeter/jtl/{}.jtl -e -o /Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/{}"
-                    .format(filename, Mytime, Mytime))
+                .format(filename, Mytime, Mytime))
             linkName = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + Mytime + '/index.html').as_uri()
-            print("已生成测试报告，具体内容可点击超链接",linkName)
-            report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/'+ Mytime).as_uri()
+            print("已生成测试报告，具体内容可点击超链接", linkName)
+            report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + Mytime).as_uri()
             print("测试报告路径：", report_path)
 
-            keyword = input("您是否需要继续运行[是/否]")
+            keyword = input("您是否需要继续运行【是/否】")
 
             while True:
                 if keyword == '是':
+                    answer = input("是否需要修改线程数【是/否】")
+                    if answer == "是":
+                        try:
+                            # 尝试获取用户输入的数字
+                            num_threads = int(input("请输入新的线程数："))
+                            # 如果输入成功，则更新线程数
+                            modify_jmx_file(filename, num_threads)
+                        except ValueError:
+                            # 如果输入的不是数字，则捕获异常并打印错误信息
+                            print("输入的不是一个有效的数字，线程数保持不变。")
+                    elif answer == "否":
+                        # 如果用户输入否，则不修改线程数，并打印信息
+                        print("不修改线程数")
+                    else:
+                        # 如果用户输入既不是是也不是否，则打印提示信息
+                        print("输入有误，请输入是或否")
+
                     new_Mytime = time.strftime("%Y-%m-%d~%H-%M-%S")
-                    new_root = Tk()
-                    new_root.withdraw()
-                    new_filename = askopenfilename()
-                    new_path = "{}".format(new_filename)
+                    new_path = "{}".format(filename)
                     if os.path.exists(new_path):
                         os.system(
                             "jmeter -n -t {} -l /Volumes/Disk/apache-jmeter-5.1.1/jmeter/jtl/{}.jtl -e -o /Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/{}"
-                                .format(new_filename, new_Mytime, new_Mytime))
-                        linkName = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime + '/index.html').as_uri()
+                            .format(filename, new_Mytime, new_Mytime))
+                        linkName = Path(
+                            '/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime + '/index.html').as_uri()
                         print("已生成测试报告，具体内容可点击超链接", linkName)
                         report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime).as_uri()
                         print("测试报告路径：", report_path)
-                        keyword = input("您是否需要继续运行[是/否]")
+                        keyword = input("您是否需要继续运行【是/否】")
                     else:
-                        print("当前目录下不存在{}.jmx文件".format(new_filename))
-                        new_path = "{}".format(new_filename)
+                        print("当前目录下不存在{}.jmx文件".format(filename))
 
                 if keyword == '否':
                     print("程序已结束,即将关闭窗口")
-                    time.sleep(3)
                     break
 
                 if keyword != '是' or '否':
-                    keyword = input("输入有误，请重新输入[是/否]")
+                    keyword = input("输入有误，请重新输入【是/否】")
             break
 
         else:
             filename = input("当前目录下不存在{}文件，请重新输入：".format(filename))
             path = "{}".format(filename)
 
-run()
 
+# 运行的脚本路径
+run('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/jmx/jsfk.jmx')
--- Jmeter/upload_load_test_generator.py
+++ Jmeter/upload_load_test_generator.py
@@ -1,18 +1,40 @@
 import os
+import re
 import time
 from pathlib import Path
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+
+def modify_jmx_file(new_num_threads):
+    # 读取JMX文件内容
+    root = Tk()
+    root.withdraw()
+    file_path = askopenfilename()
+
+    with open(file_path, 'r') as file:
+        jmx_content = file.read()
+
+    # 使用正则表达式匹配并替换数字
+    pattern = r'<stringProp name="ThreadGroup.num_threads">(\d+)</stringProp>'
+    replacement = f'<stringProp name="ThreadGroup.num_threads">{new_num_threads}</stringProp>'
+    modified_content = re.sub(pattern, replacement, jmx_content, count=1)
+
+    # 将修改后的内容写回文件
+    with open(file_path, 'w') as file:
+        file.write(modified_content)
+
+    print(f"已成功将线程数量修改为：{new_num_threads}")
+    time.sleep(3)
+
+
 def run():
     Mytime = time.strftime("%Y-%m-%d~%H-%M-%S")
-    print("程序开始运行")
     print("请选择您要运行的jmx脚本：")
     root = Tk()
     root.withdraw()
     filename = askopenfilename()
-    print("当前文件：", filename)
-    jmx_path = "/Volumes/Disk/apache-jmeter-5.1.1/jmeter/jmx"
+    print("程序开始运行")
     path = "{}".format(filename)
 
     # 判断文件是否存在
@@ -20,10 +42,10 @@ def run():
         if os.path.exists(path):
             os.system(
                 "jmeter -n -t {} -l /Volumes/Disk/apache-jmeter-5.1.1/jmeter/jtl/{}.jtl -e -o /Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/{}"
-                    .format(filename, Mytime, Mytime))
+                .format(filename, Mytime, Mytime))
             linkName = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + Mytime + '/index.html').as_uri()
-            print("已生成测试报告，具体内容可点击超链接",linkName)
-            report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/'+ Mytime).as_uri()
+            print("已生成测试报告，具体内容可点击超链接", linkName)
+            report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + Mytime).as_uri()
             print("测试报告路径：", report_path)
 
             keyword = input("您是否需要继续运行[是/否]")
@@ -38,19 +60,18 @@ def run():
                     if os.path.exists(new_path):
                         os.system(
                             "jmeter -n -t {} -l /Volumes/Disk/apache-jmeter-5.1.1/jmeter/jtl/{}.jtl -e -o /Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/{}"
-                                .format(new_filename, new_Mytime, new_Mytime))
-                        linkName = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime + '/index.html').as_uri()
+                            .format(new_filename, new_Mytime, new_Mytime))
+                        linkName = Path(
+                            '/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime + '/index.html').as_uri()
                         print("已生成测试报告，具体内容可点击超链接", linkName)
                         report_path = Path('/Volumes/Disk/apache-jmeter-5.1.1/jmeter/report/' + new_Mytime).as_uri()
                         print("测试报告路径：", report_path)
                         keyword = input("您是否需要继续运行[是/否]")
                     else:
                         print("当前目录下不存在{}.jmx文件".format(new_filename))
-                        new_path = "{}".format(new_filename)
 
                 if keyword == '否':
                     print("程序已结束,即将关闭窗口")
-                    time.sleep(3)
                     break
 
                 if keyword != '是' or '否':
@@ -58,8 +79,10 @@ def run():
             break
 
         else:
-            filename = input("当前目录下不存在{}文件，请重新输入：".format(filename))
-            path = "{}".format(filename)
+            print("未选择文件，请重新运行！")
+            break
 
-run()
 
+if __name__ == '__main__':
+    modify_jmx_file(13)
+    run()
