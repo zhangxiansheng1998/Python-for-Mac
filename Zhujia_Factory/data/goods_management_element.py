9edc24ac5bbf7629adc83ee28cdeee64f5c29601
zhangxiansheng <1120620374@qq.con>
2024-03-12 14:29:23 +0800

    Changes

--- Zhujia_Factory/data/goods_management_element.py
+++ Zhujia_Factory/data/goods_management_element.py
@@ -6,56 +6,56 @@ from selenium.webdriver.common.by import By
 '''
 
 button = \
-{
-    'goods_list': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[1]'),    # 商品列表按钮
+    {
+        'goods_list': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[1]'),  # 商品列表按钮
 
-    'add_goods': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[2]'),    # 新增商品按钮
+        'add_goods': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[2]'),  # 新增商品按钮
 
-    'goods_category': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[3]')  # 商品分类按钮
-}
+        'goods_category': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/p[3]')  # 商品分类按钮
+    }
 
 goods_list = \
-{
-    'search_input_box': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[1]/div/div/input'),  # 搜索框
+    {
+        'search_input_box': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[1]/div/div/input'),  # 搜索框
 
-    'search_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/button[1]'),  # 查询按钮
+        'search_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/button[1]'),  # 查询按钮
 
-    'reset_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/button[2]'),  # 重置按钮
+        'reset_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/button[2]'),  # 重置按钮
 
-    'goods_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[5]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[2]/div'),  # 商品名称(断言)
+        'goods_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[5]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[2]/div'),     # 商品名称(断言)
 
-    'select_goods': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[5]/div[1]/div[2]/table/thead/tr/th[1]/div/label/span/span'),  # 选中商品
+        'select_goods': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[5]/div[1]/div[2]/table/thead/tr/th[1]/div/label/span/span'),  # 选中商品
 
-    'multiple_times_shelf': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[4]/button[2]'),  # 批量上架/下架
+        'multiple_times_shelf': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[4]/button[2]'),  # 批量上架/下架
 
-    'assert_box': (By.XPATH, '/html/body/div[3]/p'),  # 文本框(断言)
+        'assert_box': (By.XPATH, '/html/body/div[3]/p'),  # 文本框(断言)
 
-    'on_sale': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[2]'),  # 在售中
+        'on_sale': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[2]'),  # 在售中
 
-    'off_sale': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[3]'),  # 已下架
-}
+        'off_sale': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[3]'),  # 已下架
+    }
 
 add_goods = \
-{
-    'goods_category': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/form[1]/div[2]/div/div/div/div/input'),    # 商品分类下拉框
+    {
+        'goods_category': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/form[1]/div[2]/div/div/div/div/input'),     # 商品分类下拉框
 
-    'goods_category_1': (By.XPATH, '/html/body/div[2]/div[3]/div/div/div[1]/ul/li[1]/span'),     # 商品分类下拉框-铝型材
+        'goods_category_1': (By.XPATH, '/html/body/div[2]/div[3]/div/div/div[1]/ul/li[1]/span'),  # 商品分类下拉框-铝型材
 
-    'goods_category_2': (By.XPATH, '/html/body/div[2]/div[3]/div/div[2]/div[1]/ul/li[1]/span'),     # 商品分类下拉框-铝合金
+        'goods_category_2': (By.XPATH, '/html/body/div[2]/div[3]/div/div[2]/div[1]/ul/li[1]/span'),  # 商品分类下拉框-铝合金
 
-    'goods_category_3': (By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/div[1]/ul/li[1]/span'),     # 商品分类下拉框-铝制用品
+        'goods_category_3': (By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/div[1]/ul/li[1]/span'),  # 商品分类下拉框-铝制用品
 
-    'goods_name': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/form[1]/div[3]/div/div[1]/div/input'),       # 商品名称
+        'goods_name': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/form[1]/div[3]/div/div[1]/div/input'),  # 商品名称
 
-    'goods_specification': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[8]/form/div/div/div[1]/div/input'),       # 商品规格
+        'goods_specification': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[8]/form/div/div/div[1]/div/input'),  # 商品规格
 
-    'add_inventory': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[11]/div[2]/div[2]/span[2]'),     # 新增库存
+        'add_inventory': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[11]/div[2]/div[2]/span[2]'),    # 新增库存
 
-    'goods_price': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[10]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[3]/div/div/div/input'),  # 商品报价-价格
+        'goods_price': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[10]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[3]/div/div/div/input'),     # 商品报价-价格
 
-    'goods_unit': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[10]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[4]/div/div/div/input'),   # 商品报价-单位名称
+        'goods_unit': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div[10]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[4]/div/div/div/input'),  # 商品报价-单位名称
 
-    'confirm': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/button'),  # 确认新增
+        'confirm': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/button'),  # 确认新增
 
-    'assert_box': (By.XPATH, '/html/body/div[3]/p')   # 文本框(断言)
-}
+        'assert_box': (By.XPATH, '/html/body/div[3]/p')  # 文本框(断言)
+    }
--- Zhujia_Factory/data/shop_management_element.py
+++ Zhujia_Factory/data/shop_management_element.py
@@ -6,104 +6,88 @@ from selenium.webdriver.common.by import By
 '''
 
 button = \
-{
-    'shop_list': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/p[1]'),    # 店铺列表按钮
+    {
+        'shop_list': (By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/p[1]'),  # 店铺列表按钮
 
-    'add_shop': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[1]')    # 新增店铺按钮
-}
+        'add_shop': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[1]')  # 新增店铺按钮
+    }
 
 shop_list = \
-{
-    'shop_status': (By.XPATH,
-                    '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                    '1]/td[5]/div/div/span'),    # 营业状态开关
+    {
+        'shop_status': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[5]/div/div/span'),       # 营业状态开关
 
-    'shop_details': (By.XPATH,
-                     '//*[@id="main"]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button['
-                     '1]/span/span'),    # 查看详情
+        'shop_details': (By.XPATH, '//*[@id="main"]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[1]/span/span'),       # 查看详情
 
-    'shop_details_return': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button/span'),    # 查看详情-返回按钮
+        'shop_details_return': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button/span'),  # 查看详情-返回按钮
 
-    'today_income': (By.XPATH,
-                     '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                     '1]/td[8]/div/button[2]/span/span'),    # 今日营业额
+        'today_income': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[2]/span/span'),       # 今日营业额
 
-    'today_income_text': (By.XPATH,'//*[@id="main"]/div[2]/div[3]/div/div/header/span'),    # 今日营业额-店铺今日统计(断言)
+        'today_income_text': (By.XPATH, '//*[@id="main"]/div[2]/div[3]/div/div/header/span'),  # 今日营业额-店铺今日统计(断言)
 
-    'today_income_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),    # 今日营业额-关闭按钮
+        'today_income_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),        # 今日营业额-关闭按钮
 
-    'custom_income': (By.XPATH,
-                      '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                      '1]/td[8]/div/button[3]/span/span'),    # 自定义收款
+        'custom_income': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[3]/span/span'),       # 自定义收款
 
-    'enter_amount': (By.XPATH, '/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div/input'),    # 自定义收款-金额输入框
+        'enter_amount': (By.XPATH, '/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div/input'),  # 自定义收款-金额输入框
 
-    'custom_income_confirm': (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[2]'),    # 自定义收款-确认按钮
+        'custom_income_confirm': (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[2]'),  # 自定义收款-确认按钮
 
-    'custom_income_cancel': (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[1]'),    # 自定义收款-取消按钮
+        'custom_income_cancel': (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[1]'),  # 自定义收款-取消按钮
 
-    'custom_income_close': (By.XPATH, '/html/body/div[3]/div/div/div[1]/button'),    # 自定义收款-关闭按钮
+        'custom_income_close': (By.XPATH, '/html/body/div[3]/div/div/div[1]/button'),  # 自定义收款-关闭按钮
 
-    'custom_income_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/p[2]'),    # 自定义收款-金额(断言)
+        'custom_income_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/p[2]'),     # 自定义收款-金额(断言)
 
-    'custom_income_close_two': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),    # 自定义收款-关闭按钮（收款码页面的关闭按钮）
+        'custom_income_close_two': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),  # 自定义收款-关闭按钮（收款码页面的关闭按钮）
 
-    'income_code': (By.XPATH,
-                    '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                    '1]/td[8]/div/button[4]/span/span'),    # 收款码
+        'income_code': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[4]/span/span'),  # 收款码
 
-    'income_code_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/span'),    # 收款码-店铺收款码(断言)
+        'income_code_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/span'),        # 收款码-店铺收款码(断言)
 
-    'income_code_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),    # 收款码-关闭按钮
+        'income_code_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),     # 收款码-关闭按钮
 
-    'shop_code': (By.XPATH,
-                  '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                  '1]/td[8]/div/button[5]/span/span'),    # 店铺码
+        'shop_code': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[5]/span/span'),  # 店铺码
 
-    'shop_code_text': (By.XPATH,'/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/span'),    # 店铺码-店铺码(断言)
+        'shop_code_text': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/span'),      # 店铺码-店铺码(断言)
 
-    'shop_code_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),    # 店铺码-关闭按钮
+        'shop_code_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button'),       # 店铺码-关闭按钮
 
-    'delete_shop': (By.XPATH,
-                    '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr['
-                    '1]/td[8]/div/button[6]/span/span'),    # 注销店铺
+        'delete_shop': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div/table/tbody/tr[1]/td[8]/div/button[6]/span/span'),  # 注销店铺
 
-    'get_code': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/form/div[2]/div/button'),    # 获取验证码
+        'get_code': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/form/div[2]/div/button'),     # 获取验证码
 
-    'enter_code': (By.XPATH,'/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/form/div['
-                            '2]/div/div/div/input'),    # 输入验证码
+        'enter_code': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/div/form/div[2]/div/div/div/input'),  # 输入验证码
 
-    'delete_shop_confirm': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/footer/span/button['
-                                      '2]/span'),    # 注销店铺-确认注销按钮
+        'delete_shop_confirm': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/footer/span/button[2]/span'),  # 注销店铺-确认注销按钮
 
-    'delete_shop_cancel': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/footer/span/button[1]'),    # 注销店铺-取消按钮
+        'delete_shop_cancel': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/footer/span/button[1]'),  # 注销店铺-取消按钮
 
-    'delete_shop_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button/i/svg'),    # 注销店铺-关闭按钮
+        'delete_shop_close': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div/header/button/i/svg'),  # 注销店铺-关闭按钮
 
-    'add_shop_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[1]'),    # 新增店铺按钮
+        'add_shop_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[1]'),  # 新增店铺按钮
 
-    'refresh_shop_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[2]')    # 刷新店铺按钮
-}
+        'refresh_shop_button': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button[2]')  # 刷新店铺按钮
+    }
 
 add_shop = \
-{
-    'platform_zhujia': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[1]/div[2]/div[1]/label/span[1]'),    # 平台类型-筑家工厂
+    {
+        'platform_zhujia': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[1]/div[2]/div[1]/label/span[1]'),    # 平台类型-筑家工厂
 
-    'platform_erp': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[1]/div[2]/div[2]/label/span[1]'),    # 平台类型-erp
+        'platform_erp': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[1]/div[2]/div[2]/label/span[1]'),  # 平台类型-erp
 
-    'shop_name': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[2]/div/div[1]/div/input'),    # 店铺名称
+        'shop_name': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[2]/div/div[1]/div/input'),     # 店铺名称
 
-    'shop_type_1': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[1]/label/span[1]/span'),    # 店铺类型-零售商
+        'shop_type_1': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[1]/label/span[1]/span'),       # 店铺类型-零售商
 
-    'shop_type_2': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[2]/label/span[1]/span'),    # 店铺类型-产品供应商
+        'shop_type_2': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[2]/label/span[1]/span'),       # 店铺类型-产品供应商
 
-    'shop_type_3': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[3]/label/span[1]/span'),    # 店铺类型-材料供应商
+        'shop_type_3': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[3]/label/span[1]/span'),       # 店铺类型-材料供应商
 
-    'shop_type_4': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[4]/label/span[1]/span'),    # 店铺类型-原材料供应商
+        'shop_type_4': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[3]/div[2]/div[4]/label/span[1]/span'),       # 店铺类型-原材料供应商
 
-    'server_phone': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[4]/div/div/div/input'),    # 客服电话
+        'server_phone': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/form/div[4]/div/div/div/input'),     # 客服电话
 
-    'logo': (By.CSS_SELECTOR, 'input[type=file]'),    # logo
+        'logo': (By.CSS_SELECTOR, 'input[type=file]'),  # logo
 
-    'submit': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button')    # 提交验证按钮
-}
+        'submit': (By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/button')  # 提交验证按钮
+    }
